399c1f6b50ac20bfb7d96c706089dcd80431788a
André Luiz Batista <andre.batista@byintera.com>
2023-01-10 16:34:50 -0300

    1.2.0

--- lib/Button.d.ts
+++ lib/Button.d.ts
@@ -1,4 +1,3 @@
-import * as React from "react";
 export declare const Button: (props: {
-    children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined;
+    children: any;
 }) => JSX.Element;
